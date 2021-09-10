5920129a1b91b741dc629e0eac025f86b316259c
codeartha <codeartha@gmail.com>
2021-09-10 14:27:16 +0200

    imported the functions in __init__.py to make them available in other programs as Functions.method_name()

---
+++ functions/__init__.py
@@ -0,0 +1,3 @@
+from .Calculations import radiologicalImpact
+from .Data_handling import db_list_of_tables, db_fetch_data, db_create_dictionary, file_create_dictionary, create_db, create_table, outputTablesRadiologicalImpact_pivoted, insert_table, write_csv
+from .Utilities import get_list_of_files
