00413a66ae02be287d442347fa1a5e04e04e973e
npaq <nicolas.paquet@tractebel.engie.com>
2021-09-24 13:56:52 +0200

    remove calculation.py

--- rdofunctions/__init__.py
+++ rdofunctions/__init__.py
@@ -1,3 +1,2 @@
-from .calculation import radiologicalImpact
 from .data_handling import db_list_of_tables, db_fetch_data, db_create_dictionary, file_create_dictionary, create_db, create_table, outputTablesRadiologicalImpact_pivoted, insert_table, write_csv
 from .utilities import get_list_of_files
