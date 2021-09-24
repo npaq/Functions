60551f4fa81e17c6001da9525d5ea8e5652099fb
npaq <89013486+npaq@users.noreply.github.com>
2021-09-24 13:12:43 +0200

    màj version test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rdofunctions',
-    version='0.0.1',
+    version='0.0.2',
     description='Private package with helper functions for radiological consequences calculations',
     url='git@github.com/npaq/Functions.git',
     author='Nicolas Paquet',
